65da2e9182af9b14d3d3360d676de373d01bdbf2
Rodrigo Rahman <rodrigorahman@academiadoflutter.com.br>
2023-10-25 15:59:44 -0300

    feat: exercise variables

---
+++ c_exercises_variables/step_01/instructions.md
@@ -0,0 +1,12 @@
+# Variáveis
+Imagine que você recebeu a tarefa de criar um algoritimo para armazenar  pacientes de um hospital.
+
+Sendo assim crie abaixo as variáveis para armazenar: 
+
+1 - Idade do paciente;
+2 - Nome Completo;
+3 - Altura do paciente;
+4 - Peso;
+
+
+## Não precisa se preocupar muito com os valores pense e tente descobrir quais seriam os tipos de cada uma das variáveis para esse algoritimo
---
+++ c_exercises_variables/step_01/solution.dart
@@ -0,0 +1,24 @@
+
+void main() {
+  
+  int idadePaciente;
+  String nomeCompleto;
+  double alturaPaciente;
+  int peso;
+
+  // Entenda o porque eu escolhi cada tipo:
+  
+  // Inteiro pois vamos armazenar o valor cheio
+  idadePaciente = 40;
+  
+  // String
+  nomeCompleto = 'Rodrigo Rahman';
+  
+  // Altura sempre utilizamos casas decimais por esse motivo um double
+  alturaPaciente = 1.70;
+
+  // Peso foi escolhido como int pois não quero armazenar as gramas, porém se você escolheu double também está correto.
+  peso = 68;
+
+
+}
